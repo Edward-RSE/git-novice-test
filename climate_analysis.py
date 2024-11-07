edbbccfdf59c2e81f9d8db7b191cd9b39ad7862d
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2024-11-07 09:44:12 +0000

    add a placeholder for rainfall processing

--- climate_analysis.py
+++ climate_analysis.py
@@ -27,3 +27,5 @@ for line in climate_data:
             kelvin = temp_conversion.fahr_to_kelvin(fahr)
 
             print(str(celsius)+", "+str(kelvin))
+
+# TODO: add a function to process rainfall
